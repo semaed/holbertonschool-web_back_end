86c1b95b91f00a912f539a642fa9a8d3c5441134
semaed <figsaned@gmail.com>
2023-08-30 13:38:37 -0400

    Correction

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Script that have a async generator"""
+"""Script for a corrutine async generator"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """generator of numbers"""
+    """Generator of numbers"""
 
     for _ in range(10):
         await asyncio.sleep(1)
