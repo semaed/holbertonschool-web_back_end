21518595881261c39b7031bc553d7d16f21de11f
semaed <figsaned@gmail.com>
2023-08-30 22:06:08 -0400

    Correction

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,3 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Collects 10 random numbers"""
     return ([i async for i in async_generator()])
-
