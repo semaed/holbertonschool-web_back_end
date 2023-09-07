5b8a0cdf473eee2dca8bb1ef6ec6c3fa44188f02
semaed <figsaned@gmail.com>
2023-09-07 11:23:33 -0400

    Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Script that change all topics of a school
+document based on the name"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics"""
+
+    query: dict = {"name": name} 
+
+    mongo_collection.update_many(query, {"$set": {"topics": topics}})
