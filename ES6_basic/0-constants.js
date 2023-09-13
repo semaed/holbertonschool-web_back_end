9fe3f2d84210cf097d114287068fa487d6f4839f
semaed <figsaned@gmail.com>
2023-09-13 08:55:24 -0400

    Const or let?

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+xport function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
