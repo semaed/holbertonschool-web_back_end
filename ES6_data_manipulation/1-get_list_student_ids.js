561aec8b9efede3f9979ef3805a161175ec3aecc
semaed <figsaned@gmail.com>
2023-10-08 09:52:03 -0400

    More mapping

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+/*enlist-disable*/
+export default function getListStudentIds(getListStudents) {
+  if (Array.isArray(getListStudents)) {
+    return getListStudents.map((student) => student.id);
+  }
+  return [];
+}
