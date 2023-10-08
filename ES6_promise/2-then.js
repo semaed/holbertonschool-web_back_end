97615a035002c00dc3ba32ac23a9171f90e206ca
semaed <figsaned@gmail.com>
2023-10-08 15:13:29 -0400

    Correction

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error())
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => ({ status: 500, body: 'Internal Server Error' }))
     .finally(() => console.log('Got a response from the API'));
 }
+
