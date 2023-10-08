f2d3d68667173eea9fd7a4e7d054b6d147081b61
semaed <figsaned@gmail.com>
2023-10-08 15:03:04 -0400

    Correction

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -3,8 +3,8 @@ export default function signUpUser(firstName, lastName) {
   return new Promise((resolve, reject) => {
     if (typeof firstName === 'string' && typeof lastName === 'string') {
       resolve({
-        firstName,
-        lastName,
+        status: 200,
+        body: 'success',
       });
     } else {
       reject(new Error('Invalid input'));
