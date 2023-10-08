1116aaca07f2dfed0eb7d96af7921155fe908360
semaed <figsaned@gmail.com>
2023-10-08 10:06:47 -0400

    Reduce

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+/*enlist-disable*/
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const reduce = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return reduce;
+}
