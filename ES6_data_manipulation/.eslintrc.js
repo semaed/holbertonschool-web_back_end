2d7ff80522cdab2aa6b0f31035ecc08216ba1254
semaed <figsaned@gmail.com>
2023-10-08 11:18:10 -0400

    Correction to file

--- ES6_data_manipulation/.eslintrc.js
+++ ES6_data_manipulation/.eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
-  // Other ESLint rules and configurations
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
   rules: {
-    'quotes': ['error', 'single'],
-    'indent': ['error', 2],
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js'],
-      },
-    },
-  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
