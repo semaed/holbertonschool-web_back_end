6fada94caa2e646260042d8cdd47f60400c131d6
semaed <figsaned@gmail.com>
2023-10-08 11:26:04 -0400

    Correction

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,7 @@
 export default function getListStudentIds(students) {
     if (!Array.isArray(students)) {
       return [];
-    }
-  
+    }  
     const map = students.map((mappedObj) => mappedObj.id);
     return map;
   }
