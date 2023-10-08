9c5fab954d4445bc95f041ba72b5d3ccbc90bc8c
semaed <figsaned@gmail.com>
2023-10-08 10:23:01 -0400

    Correction

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 /*enlist-disable*/
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
-    return '';
+    let result = '';
+    if (!startString || !startString.length) return result;
+    set.forEach((i) => {
+      if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+    });
+    return result.slice(0, result.length - 1);
   }
-
-  const string = [...set].filter((value) => value.startsWith(startString)).map((value) => value.slice(startString.length)).join('-');
-  return string;
-}
