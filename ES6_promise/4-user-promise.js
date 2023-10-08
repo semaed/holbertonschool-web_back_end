8dab6fb87efa49491fc2c5db65e67634ae3f7342
semaed <figsaned@gmail.com>
2023-10-08 15:01:07 -0400

    Correction

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,13 @@
 /* eslint-disable */
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName, lastName,
-    });
-  }
+  return new Promise((resolve, reject) => {
+    if (typeof firstName === 'string' && typeof lastName === 'string') {
+      resolve({
+        firstName,
+        lastName,
+      });
+    } else {
+      reject(new Error('Invalid input'));
+    }
+  });
+}
